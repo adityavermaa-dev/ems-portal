80607dc915a16d3128f656d1145ba83296fc8e52
adit-ya15 <aditya262701@gmail.com>
2026-07-11 15:36:41 +0530

    changed thr url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import { buildQuery } from "../utils/helpers";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL || "https://ems-portal-h3vq.onrender.com";
 
 async function request(path, options = {}) {
   const isFormData = options.body instanceof FormData;
